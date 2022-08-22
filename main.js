91cb8f2b26f9131b5311bce8bfd0be1fcb9291f3
shahinul islam <shahinul37@outlook.com>
2022-08-22 15:05:31 +0600

    selected players done

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log('added')
+const players=[]
+document.getElementById('players').addEventListener('click', function(event){
+    
+
+        if(event.target.classList[0]=='select-player'){
+            const nameValue = (event.target.parentNode.children[1].innerText);
+            if(players.length<5){
+            players.push(nameValue)
+            const ol = document.createElement("ol");
+            ol.classList.add('list-disc')
+            const li = document.createElement('li')
+            li.innerText= nameValue
+            ol.appendChild(li)
+            document.getElementById('selected-players').appendChild(ol)
+            event.target.disabled=true
+            event.target.classList.add('bg-slate-100')
+            }
+            else if(players.length==5){
+                alert('five players added')
+                console.log(players)
+            }
+        }
+        
+        else{
+            event.target.disabled=false;
+            event.target.classList.add('bg-slate-300')
+    
+        }
+
+    
+    
+})
